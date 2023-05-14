c23781065c3fe9bd0c6d0074cbda087e2b50aefd
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2023-05-14 23:37:24 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,58 +1,17 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Login Page</title>
-</head>
 <body>
-	<h1>Login Page</h1>
-	<form method="post" action="login.php">
-		<label>Username:</label>
-		<input type="text" name="username"><br>
-		<label>Password:</label>
-		<input type="password" name="password"><br>
-		<input type="submit" value="Login">
-	</form>
-</body>
-</html>
-
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-	// Connect to MySQL
-	$conn = mysqli_connect("localhost", "root", "", "example_db");
-	
-	// Check connection
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-
-	// Sanitize input data
-	$username = mysqli_real_escape_string($conn, $_POST["username"]);
-	$password = mysqli_real_escape_string($conn, $_POST["password"]);
-	
-	// Hash the password
-	$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-	
-	// Check if the user exists in the database
-	$sql = "SELECT * FROM users WHERE username='$username'";
-	$result = mysqli_query($conn, $sql);
-	
-	if (mysqli_num_rows($result) > 0) {
-		$row = mysqli_fetch_assoc($result);
-		
-		// Verify the password
-		if (password_verify($password, $row["password"])) {
-			// Login successful
-			echo "Login successful. Welcome, " . $row["username"] . "!";
-		} else {
-			// Login failed
-			echo "Invalid username or password.";
-		}
-	} else {
-		// Login failed
-		echo "Invalid username or password.";
-	}
-	
-	// Close the MySQL connection
-	mysqli_close($conn);
+if(isset($_POST['data']))
+echo $_POST['data'];
+else
+{
+?>
+<form method="post" action="">
+Enter text here:<textarea name="data"></textarea>
+<input type="submit"/>
+</form>
+<?php
 }
 ?>
+</body>
+</html>
