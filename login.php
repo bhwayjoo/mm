6042f470f72e5a2fae8e2373d10699a6ee8fa0b9
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2023-05-14 15:27:59 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,27 +1,31 @@
-<html>
-<body>
 <?php
-    if(isset($_POST['login']))
-    {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $con = mysqli_connect('localhost','root','root','sample');
-        $result = mysqli_query($con, "SELECT * FROM `users` WHERE username='$username' AND password='$password'");
-        if(mysqli_num_rows($result) == 0)
-            echo 'Invalid username or password';
-        else
-            echo '<h1>Logged in</h1><p>A Secret for you....</p>';
+session_start();
+$host = "localhost";
+$username = "<root>";
+$password = "<>";
+$database_name = "<lol>";
+$mysqli = new mysqli($host, $username, $password, $database_name);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $mysqli->query($query);
+    if ($result->num_rows == 1) {
+        $_SESSION["username"] = $username;
+        header("Location: welcome.php");
+        exit();
+    } else {
+        echo "Invalid username or password";
     }
-    else
-    {
+}
 ?>
-        <form action="" method="post">
-            Username: <input type="text" name="username"/><br />
-            Password: <input type="password" name="password"/><br />
-            <input type="submit" name="login" value="Login"/>
-        </form>
-<?php
-    }
-?>
-</body>
-</html>
+<form method="post">
+<label>Username:</label>
+<input type="text" name="username"><br>
+<label>Password:</label>
+<input type="password" name="password"><br>
+<input type="submit" value="Login">
+</form>
