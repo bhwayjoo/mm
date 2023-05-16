aa2fdd6e77ef3a730daad11162ee2b8e29613866
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2023-05-16 14:46:02 +0100

    Update mm.py

--- mm.py
+++ mm.py
@@ -8,7 +8,7 @@ def execute_command(command):
         print(f"Command execution failed with error: {e.returncode}\n{e.output}")
 
 # Example usage
-command_to_execute = """apt-get update && sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev zlib1g-dev && wget https://nginx.org/download/nginx-1.22.1.tar.gz && wget https://github.com/nbs-system/naxsi/archive/1.3.tar.gz -O naxsi-1.3.tar.gz && tar -zxvf nginx-1.22.1.tar.gz && tar -zxvf naxsi-1.3.tar.gz && cd nginx-1.22.1 && ./configure --conf-path=/etc/nginx/nginx.conf --add-module=../naxsi-1.3/naxsi_src/ \
+command_to_execute = """apt-get update && apt-get install build-essential libpcre3 libpcre3-dev libssl-dev zlib1g-dev && wget https://nginx.org/download/nginx-1.22.1.tar.gz && wget https://github.com/nbs-system/naxsi/archive/1.3.tar.gz -O naxsi-1.3.tar.gz && tar -zxvf nginx-1.22.1.tar.gz && tar -zxvf naxsi-1.3.tar.gz && cd nginx-1.22.1 && ./configure --conf-path=/etc/nginx/nginx.conf --add-module=../naxsi-1.3/naxsi_src/ \
   --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body \
   --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log \
   --http-proxy-temp-path=/var/lib/nginx/proxy --lock-path=/var/lock/nginx.lock \
