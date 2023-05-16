e4535d316427a122f978a4608c5327bcb268ab94
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2023-05-16 16:12:30 +0100

    Update mm.py

--- mm.py
+++ mm.py
@@ -8,13 +8,6 @@ def execute_command(command):
         print(f"Command execution failed with error: {e.returncode}\n{e.output}")
 
 # Example usage
-command_to_execute = """ apt-get update &&  apt-get install build-essential libpcre3 libpcre3-dev libssl-dev zlib1g-dev && wget https://nginx.org/download/nginx-1.22.1.tar.gz && wget https://github.com/nbs-system/naxsi/archive/1.3.tar.gz -O naxsi-1.3.tar.gz && tar -zxvf nginx-1.22.1.tar.gz && tar -zxvf naxsi-1.3.tar.gz && cd nginx-1.22.1 && ./configure --conf-path=/etc/nginx/nginx.conf --add-module=../naxsi-1.3/naxsi_src/ \
-  --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body \
-  --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log \
-  --http-proxy-temp-path=/var/lib/nginx/proxy --lock-path=/var/lock/nginx.lock \
-  --pid-path=/run/nginx.pid --user=www-data --group=www-data --with-http_ssl_module \
-  --without-mail_pop3_module --without-mail_smtp_module --without-mail_imap_module \
-  --without-http_uwsgi_module --without-http_scgi_module --with-openssl=../openssl && make && make install
-
+command_to_execute = """apt-get install -y apt-utils autoconf automake build-essential git libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev && git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity 
 """
 execute_command(command_to_execute)
